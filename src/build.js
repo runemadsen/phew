f9fe83c6c89797a6602cfadc93c8f95e62dbd8ae
Rune Madsen <rune@runemadsen.com>
2015-10-23 16:48:19 -0400

    first css bundling working

--- src/build.js
+++ src/build.js
@@ -1,18 +1,39 @@
 var _ = require('lodash');
 var glob = require("glob");
 var sass = require('node-sass');
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
 
-module.exports = function(argv) {
+function saveFile(filename, content) {
+  fs.writeFile("./build/" + filename, content, function(err) {
+    if(err) return console.log(err);
+    console.log("The file was saved!");
+  });
+}
 
-  // Build JS files
+function buildJS() {
 
-  // Build CSS files
+}
+
+function buildCSS() {
   glob('./stylesheets/*.scss', function(err, files) {
-
     _.each(files, function(file) {
       sass.render({ file: file }, function(err, result) {
-        console.log(result)
+        var filename = path.basename(file).replace(".scss", ".css");
+        saveFile(filename, result.css);
       });
     })
+  });
+}
+
+module.exports = function(argv) {
+  mkdirp('./build', function (err) {
+    if (err) {
+      console.error(err)
+    } else {
+      buildCSS();
+      buildJS();
+    }
   });
 }
